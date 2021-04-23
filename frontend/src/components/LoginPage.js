b4beb688e32b33052818636459b5a9458f9cfc79
redqoralsrl <redqoralsrl@gmail.com>
2021-04-23 21:17:05 +0900

    Login_frontend

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -2,8 +2,7 @@ import React, {useState} from 'react';
 // import {useDispatch} from 'react-redux';
 // import { loginUser } from '../../../_actions/user_action';
 // import { withRouter } from 'react-router-dom';
-import { Grid, Paper, Avatar } from '@material-ui/core';
-
+import { Grid, Paper, Avatar, TextField, Button, Typography, Link } from '@material-ui/core';
 
 const LoginPage = (props) => {
 
@@ -32,31 +31,69 @@ const LoginPage = (props) => {
         height : '70vh',
         width : 280,
         margin : '20px auto',
+        textAlign : 'center',
+        background : '#f6bd63',
+    }
+    const avatarStyle = {
+        width : '230px',
+        height : '90px',
+        // marginBottom: '30px',
+        margin: '10px 0 30px',
+        background : '#f6bd63',
+        // border : '1px solid black',
+    }
+    const btnstyle = {
+        margin : '8px 0',
+        fontWeight : 'bolder',
+        color : '#fff',
+        backgroundColor : '#845460'
+    }
+    const imgstyle ={
+        width : '100%',
+        height : '100%',
+    }
+    const logostyle ={
+        width: '200px',
+        marginTop: '50px',
+    }
+    const inputstyle ={
+        width : '90%',
+        margin : '0 auto',
+        height : '30px',
+        marginBottom : '20px',
+        background : '#855460',
+        color : '#fff',
     }
 
     return (
         <Grid>
             <Paper elevation={10} style={paperStyle}>
                 <Grid align="center">
-                    <Avatar></Avatar>
-                    Sign in
+                    <img src="/text_transparent.png" style={logostyle}></img>
+                    <Avatar style={avatarStyle} ><img src="/gyeran_height_cut.png" style={imgstyle} /></Avatar>
 
                 </Grid>
+                <form onSubmit={onSubmitHandler}>
+                    <input style={inputstyle} type="text" label="UserId" placeholder="Enter UserId" value={UserId} onChange={onUserIdHandler} required/>
+                    <input style={inputstyle} type="password" label="Password" placeholder="Enter Password" value={Password} onChange={onPasswordHandler} required/>
+                    {/* <FormControlLabel 
+                        control={
+                            <Checkbox
+                                name="checkedB"
+                                color="primary"
+                            />
+                        }
+                        label="Remember Me"
+                    /> */}
+                    <Button type="submit" style={btnstyle} variant="contained" fullWidth>Sign in</Button>
+                </form>
+                <Typography>
+                    <Link href="#">
+                        Sign up
+                    </Link>
+                </Typography>
             </Paper>
         </Grid>
-        // <div>
-        //     <form onSubmit={onSubmitHandler}>
-        //         <label>UserId</label>
-        //         <input type="text" value={UserId} onChange={onUserIdHandler} />
-        //         <label>Password</label>
-        //         <input type="password" value={Password} onChange={onPasswordHandler} />
-        //         <br />
-        //         <button type="submit">
-        //             login
-        //         </button>
-        //     </form>
-        // </div>
-
     )
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,25 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// import { Provider } from 'react-redux';
-// import { applyMiddleware, createStore } from 'redux';
-// import promiseMiddleware from 'redux-promise';
-// import ReduxThunk from 'redux-thunk';
-// import Reducer from './_reducers';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import Reducer from './api/reducers';
 
 // 스토어 만들기
-// const Store = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
+const Store = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
 
 ReactDOM.render(
-  // <Provider
-  //   store = {Store(Reducer,
-  //       window.__REDUX_DEVTOOLS_EXTENSION__ &&
-  //       window.__REDUX_DEVTOOLS_EXTENSION__()
-  //     )}
-  // >
-
+  <Provider
+    store = {Store(Reducer,
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+      )}
+  >
     <App />,
-  // </Provider>,
+  </Provider>,
   document.getElementById('root')
 );
 
