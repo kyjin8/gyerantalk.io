4b408b8f456f87663d0357153b1fdfebea52f10b
redqoralsrl <redqoralsrl@gmail.com>
2021-04-26 11:28:49 +0900

    leftTitle cursor

--- frontend/src/components/LeftPage/FriendList/PlusFriend/PlusFriend.js
+++ frontend/src/components/LeftPage/FriendList/PlusFriend/PlusFriend.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
 
-const PlusFriend = () => {
+const PlusFriend = ({onToggle3}) => {
 
     return (
         <div className="plus_site">
-            <AddCircleOutlineIcon className="plus_icon"/>
+            <AddCircleOutlineIcon className="plus_icon" onClick={onToggle3}/>
             <div>친구추가</div>
         </div>
     )
--- frontend/src/components/LeftPage/FriendList/Title/LeftTitle.js
+++ frontend/src/components/LeftPage/FriendList/Title/LeftTitle.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useRef, useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
 import '../../../MainPage/Main.scss';
 import {
@@ -7,42 +7,33 @@ import {
     UserAddOutlined
 } from '@ant-design/icons';
 
-const LeftTitle = () => {
-
-    const [Search, setSearch] = useState(false);
-    const [Friend, setFriend] = useState(false);
-    const [InputText, setInputText] = useState("");
-    const [InputText2, setInputText2] = useState("");
+const LeftTitle = ({
+    Search,
+    Friend,
+    FriendClick,
+    InputText,
+    InputText2,
+    onToggle,
+    onToggle2,
+    onInputTextHandler,
+    onInputTextHandler2
+}) => {
 
     const inputEl = useRef(null);
     const inputEl2 = useRef(null);
 
-    const onToggle = () =>{
-        setSearch(!Search);
-        setFriend(false);
-    }
-    const onToggle2 = () =>{
-        setFriend(!Friend);
-        setSearch(false);
-    }
-    const onInputTextHandler = (e) =>{
-        setInputText(e.target.value);
-    }
-    const onInputTextHandler2 = (e) =>{
-        setInputText2(e.target.value);
-    }
-
     useEffect(() => {
         if(Search) inputEl.current.focus();
         if(Friend) inputEl2.current.focus();
-    }, [Search, Friend])
+        if(FriendClick) inputEl2.current.focus();
+    }, [Search, Friend, FriendClick])
 
     return (
         <div className="left_title">
             <div>친구</div>
             <div className="search_friend">
                 {Search ? <input type="text" placeholder="친구검색" value={InputText} ref={inputEl} onChange={onInputTextHandler}/> : null}
-                {Friend ? <input type="text" placeholder="친구추가" value={InputText2} ref={inputEl2} onChange={onInputTextHandler2}/> : null}
+                {Friend || FriendClick ? <input type="text" placeholder="친구추가" value={InputText2} ref={inputEl2} onChange={onInputTextHandler2}/> : null}
                 <SearchOutlined onClick={onToggle}/>
                 <UserAddOutlined onClick={onToggle2} className="add_friend"/>
             </div>
