dcc5ff9975ba3792160de5bd37cc4e57154150f0
redqoralsrl <redqoralsrl@gmail.com>
2021-04-27 15:25:39 +0900

    Internet design

--- frontend/src/components/LeftPage/Internet/Internet.js
+++ frontend/src/components/LeftPage/Internet/Internet.js
@@ -1,13 +1,19 @@
-import React from 'react'
-import IntTitle from './IntTitle/'
+import React from 'react';
+import IntTitle from './IntTitle/IntTitle';
+import { withRouter } from 'react-router-dom';
+import News from './News/News';
+import Shop from './Shop/Shop';
 
-const Internet = () => {
+const Internet = ({match, Userdata}) => {
+    const data = match.params.search || 'News';
     return (
         <div>
             <IntTitle />
+            {data === 'News' ? <News /> : null}
+            {data === 'Shop' ? <Shop /> : null}
         </div>
     )
 }
 
-export default Internet;
+export default withRouter(Internet);
  
---
+++ frontend/src/components/LeftPage/Internet/Shop/Shop.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+
+const Shop = () => {
+    return (
+        <div>
+            쇼핑페이지
+        </div>
+    )
+}
+
+export default withRouter(Shop);
